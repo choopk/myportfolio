cc2577f80fce2513b10984e9d4659d2ec8e87ef6
unknown <choopk1994@gmail.com>
2018-10-13 00:08:26 +0800

    .

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+    constructor(props {
+        
+    })
     render() {
         return (
             <div><h1>Projects Page</h1></div>
