860d273cc511a26b9b8d2794269c5b88b2aae9d3
unknown <choopk1994@gmail.com>
2018-10-13 21:50:40 +0800

    added project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardMenu, CardActions, Button, CarMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
-    constructor(props {
-        
-    })
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (
+                <Card shadow={5} style={{ mindIwidth: '450', margin: 'auto' }}>
+                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://teamairship.com/wp-content/uploads/2017/10/react-native-workshop.jpg) center / cover' }}></CardTitle>
+                    <CardText>
+                        werewr
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Video</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: '#fff' }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            )
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div><h1>This is Angular</h1></div>
+            )
+        } else if (this.state.activeTab === 2) {
+            return (
+                <div><h1>This is VueJS</h1></div>
+            )
+        } else if (this.state.activeTab === 3) {
+            return (
+                <div><h1>This is MongoDB</h1></div>
+            )
+        }
+    }
+
+
+
     render() {
+
         return (
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                </Tabs>
+
+                <section className="projects-grid">
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+
+
+                </section>
+            </div>
         )
     }
 }
