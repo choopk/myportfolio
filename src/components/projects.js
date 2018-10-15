246eeff0925f813b8d54a47357dda835e3852e4f
unknown <choopk1994@gmail.com>
2018-10-15 20:15:47 +0800

    added resume page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardMenu, CardActions, Button, IconButton } from 'react-mdl';
+import avatar from '../image/AcControl.png';
 
 class Projects extends Component {
     constructor(props) {
@@ -10,19 +11,62 @@ class Projects extends Component {
     toggleCategories() {
         if (this.state.activeTab === 0) {
             return (
+                <div className="projects-grid">
                 <Card shadow={5} style={{ mindIwidth: '450', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://teamairship.com/wp-content/uploads/2017/10/react-native-workshop.jpg) center / cover' }}></CardTitle>
-                    <CardText>
-                        werewr
+                        <CardTitle style={{ color: '#fff', height: '500px', background: 'url(https://i.imgur.com/m02TAYE.png)' }}></CardTitle>
+                        <CardText>
+                            werewr
                     </CardText>
-                    <CardActions border>
-                        <Button colored>GitHub</Button>
+                        <CardActions border>
+
+                            {/* Github 
+                     <Button colored>GitHub</Button>
                         <Button colored>Video</Button>
-                    </CardActions>
-                    <CardMenu style={{ color: '#fff' }}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                </Card>
+                    */}
+
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+
+
+                        </CardMenu>
+                    </Card>
+                    <Card shadow={5} style={{ mindIwidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://teamairship.com/wp-content/uploads/2017/10/react-native-workshop.jpg) center / cover' }}></CardTitle>
+                        <CardText>
+                            werewr
+                    </CardText>
+                        <CardActions border>
+
+                            {/* Github 
+                     <Button colored>GitHub</Button>
+                        <Button colored>Video</Button>
+                    */}
+
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+
+
+                        </CardMenu>
+                    </Card>
+                    <Card shadow={5} style={{ mindIwidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://teamairship.com/wp-content/uploads/2017/10/react-native-workshop.jpg) center / cover' }}></CardTitle>
+                        <CardText>
+                            werewr
+                    </CardText>
+                        <CardActions border>
+
+                            {/* Github 
+                     <Button colored>GitHub</Button>
+                        <Button colored>Video</Button>
+                    */}
+
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+
+
+                        </CardMenu>
+                    </Card>
+                </div>
             )
         } else if (this.state.activeTab === 1) {
             return (
@@ -46,13 +90,13 @@ class Projects extends Component {
         return (
             <div className="category-tabs">
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>React</Tab>
+                    <Tab>Smart Home App</Tab>
                     <Tab>React</Tab>
                     <Tab>React</Tab>
                     <Tab>React</Tab>
                 </Tabs>
 
-                <section className="projects-grid">
+                <section>
                     <Grid>
                         <Cell col={12}>
                             <div className="content">{this.toggleCategories()}</div>
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import avatar from '../image/avatar.gif';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
 
 class Resume extends Component {
     render() {
         return (
             <div>
                 <Grid>
-                    <Cell col={4}>Left Side</Cell>
-                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
+                    <Cell col={4}>
+                        <div style={{ textAlign: 'center' }}>
+                            <img className="avatar-resume"
+                                src={avatar}
+                                alt="avatar"
+                            />
+                        </div>
+                        <h2 style={{ paddingTop: '0.5em' }}>Choo Jian Cheng</h2>
+                        <h4 style={{ color: 'grey' }}>Programmer and Engineer</h4>
+                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                        <p>
+                            sdfdsfdsfsdfjierjoiwerjwoeij
+                            ofiwejurowerweirwer
+                            woerweorwre
+                    </p>
+                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }}></hr>
+                        <h5>Address</h5>
+                        <p>36, Lorong Permai 3, Taman Bukit Gambir, Gelugor, 11700 Pulau Pinang.</p>
+                        <h5>Phone</h5>
+                        <p>019-8501480</p>
+                        <h5>Email</h5>
+
+                    </Cell>
+                    <Cell className="resume-right-col" col={8}>
+                        <h2>Education</h2>
+                        <Education
+                            startYear={2012}
+                            endYear={2013}
+                            schoolName="Inti College"
+                            schoolDescription="High"
+                        />
+                        <Education
+                            startYear={2007}
+                            endYear={2009}
+                            schoolName="KDU University College"
+                            degreeTaken="Higher School Certificate"
+                        />
+                        <hr style={{ borderTop: '3px solid #e22947' }}></hr>
+                        <h2>Experience</h2>
+                        <Experience
+                            startYear={2017}
+                            endYear={2018}
+                            jobName="First Job"
+                            jobDescription="sdfssffdsfsfsdfdsf
+                            sdfdsfsdfsdfsdsfd"
+                        />
+                        <hr style={{ borderTop: '3px solid #e22947' }} />
+                        <h2>Skills</h2>
+                        <Skills 
+                            skill="javascript"
+                            progress={70}
+                        />
+                        <Skills 
+                            skill="HTML/CSS"
+                            progress={40}
+                        />
+                        <Skills 
+                            skill="NodeJS"
+                            progress={40}
+                        />
+                        <Skills 
+                            skill="React"
+                            progress={50}
+                        />
+                            <Skills 
+                            skill="C/C++"
+                            progress={40}
+                        />
+                    </Cell>
+
                 </Grid>
             </div>
         )
